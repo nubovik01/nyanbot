8c41802a96ee7e9d137996457ddbe47cf74417ba
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-11-11 16:43:27 +0300

    v0.4.8.b6.42⌂: copyright

--- src/utils/CommandsArgsParser.js
+++ src/utils/CommandsArgsParser.js
@@ -7,7 +7,7 @@
 //
 // Code is licensed under MIT unless otherwise specified.
 // https://opensource.org/license/mit/
-// (c) t.me/qwkrtezzz (https://github.com/nubovik01)
+// (c) qwkrtezzz (https://github.com/nubovik01)
 
 module.exports = (command = "") => {
   if (command.indexOf(" ") === -1) return [];
