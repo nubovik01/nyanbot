d4c894de2d6690345f0827ee6459ff5697ccf1c7
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-11-11 16:42:51 +0300

    v0.4.8.b6.38⌂: copyright

--- src/commands_slash/Info/version.js
+++ src/commands_slash/Info/version.js
@@ -7,7 +7,7 @@
 //
 // Code is licensed under MIT unless otherwise specified.
 // https://opensource.org/license/mit/
-// (c) t.me/qwkrtezzz (https://github.com/nubovik01)
+// (c) qwkrtezzz (https://github.com/nubovik01)
 
 const { version, dependencies } = require('../../../package.json');
 const { VERSION_MANAGEMENT_SYMBOL } = require('../../../config.js');
