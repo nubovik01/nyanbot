ef943b709f772542048be25fa2cb8abec143cb7b
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-11-11 16:43:07 +0300

    v0.4.8.b6.40⌂: copyright

--- src/events/botReady.js
+++ src/events/botReady.js
@@ -7,7 +7,7 @@
 //
 // Code is licensed under MIT unless otherwise specified.
 // https://opensource.org/license/mit/
-// (c) t.me/qwkrtezzz (https://github.com/nubovik01)
+// (c) qwkrtezzz (https://github.com/nubovik01)
 
 module.exports.run = async (message, client) => {
   console.log('Logged in as %s', client.user.tag);
