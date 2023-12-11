64e9740a7e643ca10a9feb6c0f7977755e630e7d
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-12-12 00:20:16 +0300

    v0.4.9⌂: micro-update

--- src/commands/Bot/version.js
+++ src/commands/Bot/version.js
@@ -12,13 +12,18 @@
 const { version, dependencies } = require('../../../package.json');
 const { VERSION_MANAGEMENT_SYMBOL } = require('../../../config.js');
 const emojis = require('../../../emojis.js');
+const { oneLine } = require('common-tags');
 
 module.exports.run = async (client, message, db, args) => {
   const botVersion = `${version}${VERSION_MANAGEMENT_SYMBOL}`;
   const discordJsVersion = dependencies['discord.js'].substring(1);
+  const postgresqlVersion = (await db.getVersion()).split(" ");
 
   return message.channel.send({
-    content: `v${botVersion} ${emojis.DEFAULT.CAT} | ${process.version} ${emojis.CUSTOM.NODEJS} | v13.4 ${emojis.CUSTOM.POSTGRESQL} | v${discordJsVersion} ${emojis.CUSTOM.DISCORD}`
+    content: oneLine`v${botVersion} ${emojis.DEFAULT.CAT} |
+    ${process.version} ${emojis.CUSTOM.NODEJS} |
+    v${postgresqlVersion[1]} ${emojis.CUSTOM.POSTGRESQL} |
+    v${discordJsVersion} ${emojis.CUSTOM.DISCORD}`
   });
 };
 
--- src/commands_slash/Bot/version.js
+++ src/commands_slash/Bot/version.js
@@ -12,13 +12,18 @@
 const { version, dependencies } = require('../../../package.json');
 const { VERSION_MANAGEMENT_SYMBOL } = require('../../../config.js');
 const emojis = require('../../../emojis.js');
+const { oneLine } = require('common-tags');
 
 module.exports.run = async (client, interaction, command, subCommand, db, arguments) => {
   const botVersion = `${version}${VERSION_MANAGEMENT_SYMBOL}`;
   const discordJsVersion = dependencies['discord.js'].substring(1);
+  const postgresqlVersion = (await db.getVersion()).split(" ");
 
-  return interaction.reply({
-    content: `v${botVersion} ${emojis.DEFAULT.CAT} | ${process.version} ${emojis.CUSTOM.NODEJS} | v13.4 ${emojis.CUSTOM.POSTGRESQL} | v${discordJsVersion} ${emojis.CUSTOM.DISCORD}`
+  return message.channel.send({
+    content: oneLine`v${botVersion} ${emojis.DEFAULT.CAT} |
+    ${process.version} ${emojis.CUSTOM.NODEJS} |
+    v${postgresqlVersion[1]} ${emojis.CUSTOM.POSTGRESQL} |
+    v${discordJsVersion} ${emojis.CUSTOM.DISCORD}`
   });
 };
 
